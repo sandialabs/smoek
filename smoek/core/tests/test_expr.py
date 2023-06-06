5f208d9a34fc499bb274dc521071542d0284aafb
William Hart <whart222@gmail.com>
2023-06-06 14:54:22 -0400

    More tests

--- smoek/core/tests/test_expr.py
+++ smoek/core/tests/test_expr.py
@@ -28,6 +28,21 @@ def test_sum_simple():
     e = a+b
     assert e.to_list() == ["+", "a", "b"]
 
+def test_sum_const():
+    a = variable(name="a")
+
+    e = 5+a
+    assert e.to_list() == ["+", "5", "a"]
+
+    e = a+5
+    assert e.to_list() == ["+", "a", "5"]
+
+    e = 5.0+a
+    assert e.to_list() == ["+", "5.0", "a"]
+
+    e = a+5.0
+    assert e.to_list() == ["+", "a", "5.0"]
+
 def test_sum_nested():
     a = variable(name="a")
     b = variable(name="b")
@@ -85,19 +100,103 @@ def test_sum_nested():
     #
     assert e.to_list() == ["+", ["+", "a", "b"], ["+", "c", "d"]]
 
-def test_sum_const():
+    #           +
+    #          / \
+    #         *   c
+    #        / \
+    #       2   +
+    #          / \
+    #         a   b
+    e1 = a + b
+    e = 2 * e1 + c
+    #
+    assert e.to_list() == ["+", ["*", "2", ["+", "a", "b"]], "c"]
+
+    #         *
+    #        / \
+    #       3   +
+    #          / \
+    #         *   c
+    #        / \
+    #       2   +
+    #          / \
+    #         a   b
+    e1 = a + b
+    e = 3 * (2 * e1 + c)
+    #
+    assert e.to_list() == ["*", "3", ["+", ["*", "2", ["+", "a", "b"]], "c"]]
+
+    #       +
+    #      / \
+    #     *   b
+    #    / \
+    #   a   5
+    e1 = a * 5
+    e = e1 + b
+    #
+    assert e.to_list() == ["+", ["*", "a", "5"], "b"]
+
+    #       +
+    #      / \
+    #     b   *
+    #        / \
+    #       a   5
+    e1 = a * 5
+    e = b + e1
+    #
+    assert e.to_list() == ["+", "b", ["*", "a", "5"]]
+
+    #            +
+    #          /   \
+    #         *     +
+    #        / \   / \
+    #       a   5 b   c
+    e1 = a * 5
+    e2 = b + c
+    e = e1 + e2
+    #
+    assert e.to_list() == ["+", ["*", "a", "5"], ["+", "b", "c"]]
+
+    #            +
+    #          /   \
+    #         +     *
+    #        / \   / \
+    #       b   c a   5
+    e2 = b + c
+    e = e2 + e1
+    #
+    assert e.to_list() == ["+", ["+", "b", "c"], ["*", "a", "5"]]
+
+def test_trivialSum():
+    #
+    # Check that adding zero doesn't change the expression
+    #
     a = variable(name="a")
 
-    e = 5+a
-    assert e.to_list() == ["+", "5", "a"]
+    e = a + 0
+    assert e.to_list() == "a"
 
-    e = a+5
-    assert e.to_list() == ["+", "a", "5"]
+    e = 0 + a
+    assert e.to_list() == "a"
 
-    e = 5.0+a
-    assert e.to_list() == ["+", "5.0", "a"]
+    e = a + 0.0
+    assert e.to_list() == "a"
 
-    e = a+5.0
-    assert e.to_list() == ["+", "a", "5.0"]
+    e = 0.0 + a
+    assert e.to_list() == "a"
 
+    e = a
+    e += 0
+    assert e.to_list() == "a"
+
+    e = a
+    e += 0.0
+    assert e.to_list() == "a"
+
+    #
+    # Adding zero to a sum will not change the sum
+    #
+    e = a + a
+    f = e + 0
+    assert f.to_list() == ["+", "a", "a"]
 
