597336ee81402cb3660c7fc3f7dce53090414ad6
llueg <llueg@andrew.cmu.edu>
2024-04-11 16:29:04 -0400

    added to expr_components, moved Expression to separate file

--- smoek/core/expr_components.py
+++ smoek/core/expr_components.py
@@ -1,26 +1,193 @@
-from .components import ModelingComponent
+# todo: error checking
+class ExprNode(object):
+    def __add__(self, right):
+        right = _wrap_expression_if_needed(right)
+        
+        # don't do the addition if right is zero
+        if isinstance(right, NumberWrapper) and right._value == 0:
+            return self
 
-class Expression(ModelingComponent):
-    def __init__(self, name=None, expr=None, doc=None):
-        super().__init__(name=name, doc=doc)
-        assert expr is not None
-        self._expr = expr
+        return BinaryExprNode(self, right, '+')
+
+    def __radd__(self, left):
+        left = _wrap_expression_if_needed(left)
+
+        # don't do the addition if right is zero
+        if isinstance(left, NumberWrapper) and left._value == 0:
+            return self
+
+        return BinaryExprNode(left, self, '+')
+
+    def __mul__(self, right):
+        right = _wrap_expression_if_needed(right)
+        
+        # don't do the multiplication if right is zero
+        if isinstance(right, NumberWrapper):
+            if right._value == 0:
+                return right
+            elif right._value == 1:
+                return self
+            
+        return BinaryExprNode(self, right, '*')
+
+    def __rmul__(self, left):
+        left = _wrap_expression_if_needed(left)
+
+        # don't do the addition if right is zero
+        if isinstance(left, NumberWrapper):
+            if left._value == 0:
+                return left
+            elif left._value == 1:
+                return self
+        
+        return BinaryExprNode(left, self, '*')
+    
+    def __sub__(self, right):
+        right = _wrap_expression_if_needed(right)
+        
+        # don't do the addition if right is zero
+        if isinstance(right, NumberWrapper) and right._value == 0:
+            return self
+
+        return BinaryExprNode(self, right, '-')
+    
+    def __rsub__(self, left):
+        left = _wrap_expression_if_needed(left)
+
+        # don't do the addition if right is zero
+        if isinstance(left, NumberWrapper) and left._value == 0:
+            return self
+
+        return BinaryExprNode(left, self, '-')
+    
+    def __truediv__(self, right):
+        right = _wrap_expression_if_needed(right)
+        
+        # don't do the multiplication if right is zero
+        if isinstance(right, NumberWrapper):
+            if right._value == 0:
+                raise ZeroDivisionError('division by zero')
+            elif right._value == 1:
+                return self
+            
+        return BinaryExprNode(self, right, '/')
+    
+    def __rtruediv__(self, left):
+        left = _wrap_expression_if_needed(left)
+
+        # don't do the addition if left is zero
+        if isinstance(left, NumberWrapper) and left._value == 0:
+            return NumberWrapper(0)
+
+        return BinaryExprNode(left, self, '/')
+
+    def __le__(self, right):
+        return BinaryExprNode(self, _wrap_expression_if_needed(right), '<=')
+
+    def __ge__(self, right):
+        return BinaryExprNode(self, _wrap_expression_if_needed(right), '>=')
+
+    def __eq__(self, right):
+        return BinaryExprNode(self, _wrap_expression_if_needed(right), '==')
+    
+    def __pow__(self, right):
+        right = _wrap_expression_if_needed(right)
+        
+        # don't do the multiplication if right is zero
+        if isinstance(right, NumberWrapper):
+            if right._value == 0:
+                return NumberWrapper(1)
+            elif right._value == 1:
+                return self
+            
+        return BinaryExprNode(self, _wrap_expression_if_needed(right), '**')
+
+    # def to_list(self):
+    #     return smoek.core.utils.to_list(self)
+    
+    # def to_string(self):
+    #     return smoek.core.utils.to_string(self)
+
+class ExprLeaf(ExprNode):
+    def __init__(self):
+        pass
+
+    def to_string(self):
+        raise NotImplementedError('Derived classes must implement this')
+
+class BinaryExprNode(ExprNode):
+    def __init__(self, left, right, operation):
+        assert isinstance(left, ExprNode)
+        assert isinstance(right, ExprNode)
+        self._left = left
+        self._right = right
+        self._operation = operation
 
     @property
-    def expr(self):
-        return self._expr
+    def left(self):
+        return self._left
 
-class Constraint(Expression):
-    def __init__(self, name=None, expr=None, doc=None):
-        super().__init__(name=name, expr=expr, doc=doc)
+    @property
+    def right(self):
+        return self._right
 
-def constraint(name=None, expr=None, doc=None):
-    return Constraint(name=name, expr=expr, doc=doc)
-
-class Objective(Expression):
-    def __init__(self, name=None, expr=None, doc=None):
-        super().__init__(name=name, expr=expr, doc=doc)
-
-def objective(name=None, expr=None, doc=None):
-    return Objective(name=name, expr=expr, doc=doc)
+    @property
+    def operation(self):
+        return self._operation
     
+class UnaryExprNode(ExprNode):
+    def __init__(self, expr, operation):
+        assert isinstance(expr, ExprNode)
+        self._arg = expr
+        self._operation = operation
+
+    @property
+    def arg(self):
+        return self._arg
+
+    @property
+    def operation(self):
+        return self._operation
+    
+    # def to_string(self):
+    #     return '{}({})'.format(self._operation, self._arg.to_string())
+
+class NumberWrapper(ExprLeaf):
+    def __init__(self, value):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    def to_string(self):
+        return '{}'.format(self._value)
+
+def _wrap_expression_if_needed(expr):
+    if not isinstance(expr, ExprNode):
+        if isinstance(expr, float) or isinstance(expr, int):
+            return NumberWrapper(expr)
+        else:
+            raise TypeError(f"unsupported operand type(s) in expression {expr} of type {type(expr)}")
+
+    return expr
+
+class ComponentIndicesNode(ExprLeaf):
+    def __init__(self, component, indices):
+        self._component = component
+        if type(indices) is tuple:
+            self._indices = indices
+        else:
+            self._indices = (indices,)
+
+    @property
+    def component(self):
+        return self._component
+    
+    @property
+    def indices(self):
+        return self._indices
+    
+    def to_string(self):
+        return f'{self._component.name}[{", ".join([index.to_string() for index in self._indices])}]'
+
--- smoek/core/functions.py
+++ smoek/core/functions.py
@@ -1,4 +1,4 @@
-from smoek.core.expression import ExprNode
+from smoek.core.expr_components import ExprNode, UnaryExprNode
 from smoek.core.components import ForAllObject
 
 class SumExprNode(ExprNode):
@@ -13,6 +13,37 @@ class SumExprNode(ExprNode):
         self._forall.forall(index, In=In)
         return self
 
-def smoek_sum(expr):
+def sum(expr):
     return SumExprNode(expr=expr)
-    
+
+
+class ProdExprNode(ExprNode):
+    def __init__(self, expr):
+        assert isinstance(expr, ExprNode)
+        self._expr = expr
+        self._forall = None
+
+    def forall(self, index, In=None):
+        if self._forall is None:
+            self._forall = ForAllObject()
+        self._forall.forall(index, In=In)
+        return self
+
+def prod(expr):
+    return ProdExprNode(expr=expr)
+
+def log(expr):
+    return UnaryExprNode(expr=expr, operation='log')
+
+def exp(expr):
+    return UnaryExprNode(expr=expr, operation='exp')
+
+def sin(expr):
+    return UnaryExprNode(expr=expr, operation='sin')
+
+def cos(expr):
+    return UnaryExprNode(expr=expr, operation='cos')
+
+def tan(expr):
+    return UnaryExprNode(expr=expr, operation='tan')
+
