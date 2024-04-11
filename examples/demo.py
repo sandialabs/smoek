4cce8ae98977d0eaeba0987df4e580b30b446fa5
llueg <llueg@andrew.cmu.edu>
2024-04-11 16:33:16 -0400

    rewrote latex writer, added small dem

---
+++ examples/demo.py
@@ -0,0 +1,60 @@
+import smoek.core as smk
+import numpy as np
+from smoek.io.tex import expression_to_latex_string, LatexWriter
+
+# indices
+i = smk.index('i')
+j = smk.index('j')
+
+# index sets, contents/constructiuon not defined
+# should there be a data attribute?
+I = smk.set('I')
+J = smk.set('J')
+
+# indexed sets, contents/constructiuon not defined
+S = smk.set('S').forall(i, In=I).suchthat(i >= 0)
+
+
+M = smk.parameter('M').forall(i, In=I).forall(j, In=J)
+
+
+v = smk.variable(name='v').forall(i, In=S)
+x = smk.variable(name='x').forall(j, In=J)
+z = smk.variable(name='z', domain=smk.Domain.Binary).forall(i, In=S)
+
+c1 = smk.constraint(expr = 3 * v[i] - smk.sin(x[j] + 4) >= 0, name='c1').forall(i, In=S).forall(j, In=J)
+
+c2 = smk.constraint(expr = M[i, j] / x[j] <= v[i] * z[i], name='c2').forall(i, In=I).forall(j, In=J)
+
+c3 = smk.constraint(expr = smk.sum(v[i]**2 + x[j]**2).forall(i, In=I).forall(j, In=J) <= 100, name='c3')
+
+# print(smk.to_string(c1._expr))
+# print(smk.to_list(c1._expr))
+print(c1.to_string())
+print(c2.to_string())
+print(c3.to_string())
+print(smk.utils.expr_to_string(c1._expr))
+print(smk.utils.expr_to_string(c2._expr))
+print(smk.utils.expr_to_string(c3._expr))
+
+print(expression_to_latex_string(c1._expr))
+print(expression_to_latex_string(c2._expr))
+print(expression_to_latex_string(c3._expr))
+
+# print(smk.utils.expr_to_list(c1._expr))
+# print(smk.utils.expr_to_list(c2._expr))
+# print(smk.utils.expr_to_list(c3._expr))
+
+# ex1 = smk.expression(name='ex1', expr=v[i]**3 + smk.sin(x[j] + 4))
+# ex2 = smk.expression(name='ex2', expr=3 * ex1)
+# print(smk.utils.expr_to_string(ex2))
+
+m = smk.model(minimize = smk.sum(v[i]**2 + x[j]**2).forall(i, In=I).forall(j, In=J),
+              constraints=[c1, c2],
+              variables=[v, x, z],
+              name='m')
+
+LatexWriter()._write_model(m, 'examples/tex/model.tex')
+
+
+
--- smoek/io/tex.py
+++ smoek/io/tex.py
@@ -1,123 +1,206 @@
 import os
-import sympy as sp
+# import sympy as sp
 
-from jinja2 import Environment, FileSystemLoader
+# from jinja2 import Environment, FileSystemLoader
 
 # TODO: remove the reliance on types
-from smoek.core.set_components import Set # ScalarSet, IndexedSet
-from smoek.core.var_components import ScalarVariable # ScalarVariable, IndexedVariable
-from smoek.core.expr_components import Expression, Constraint, Objective # ScalarExpression, IndexedExpression
-from smoek.core.components import ComponentIndicesNode
-from smoek.core.data_components import Parameter # ScalarParameter, IndexedParameter
-
-from smoek.core.expression import ExprLeaf, BinaryExprNode
+from smoek.core.expr_components import ExprLeaf, BinaryExprNode, ComponentIndicesNode, UnaryExprNode
+from smoek.core.expressions import Expression
+from smoek.core.functions import SumExprNode, ProdExprNode
 from smoek.core.utils import BottomUpDepthFirstExpressionWalker
-from smoek.core.functions import SumExprNode
+from smoek.core.model_components import Model
+from smoek.core.var_components import ScalarVariable, IndexedVariable, Domain
 
-def _latex_expression(expr):
-    w = LatexExpressionWalker()
-    return w.latex_expression(expr)
 
-class LatexWriter(object):
-    def __init__(self):
-        pass
-
-    def write_model(self, fname, *args):
-        file_path = os.path.dirname(os.path.abspath(__file__))
-        templates_path = os.path.join(file_path, 'templates')
-        environment = Environment(loader=FileSystemLoader(templates_path))
-        template = environment.get_template("tex_template.tex")
-        symbstr = ''
-        constr = ''
-        for c in args:
-            if isinstance(c, Set) or isinstance(c, ScalarVariable) or isinstance(c, Parameter):
-                if c.is_scalar():
-                    symbstr += f'${c.name}$ & {c.doc}\\\\ \n'
-                else:
-                    indices = list(idx.name for idx in c._forall.indices_list())
-                    indices = ','.join(indices)
-                    symbstr += f'${c.name}_{{{indices}}}$ & {c.doc}\\\\ \n'
-            elif isinstance(c, Expression):
-                if c.is_scalar():
-                    expr = _latex_expression(c.expr)
-                    constr += f'\\begin{{align}}\n{expr}\n\\end{{align}}\n'
-                else:
-                    expr = _latex_expression(c.expr)
-                    constr += f'\\begin{{align}}\n{expr} \;\; \forall i \in S\n\\end{{align}}\n'
-            else:
-                raise NotImplementedError(f'Unknown expression {expr} of type {type(expr)} in LatexWriter')
-
-        content = template.render(symbols=symbstr, constraints=constr)
-        with open(fname, mode="w", encoding="utf-8") as texfile:
-            texfile.write(content)
-    
-    def _write_model(self, fname, *args):
-        with open(fname, 'w') as fd:
-            # write the notation table
-            fd.write(r"""\documentclass{article}
-\usepackage{amsmath}
-\begin{document}
-\begin{table}
-\begin{tabular}{c|p{4in}}
-Symbol & Description \\
-\hline \hline
-"""
-                     )
-                     
-            for c in args:
-                if isinstance(c, ScalarSet) or isinstance(c, ScalarVariable) or isinstance(c, ScalarParameter): 
-                    fd.write(f'${c.name}$ & docs\\\\')
-                elif isinstance(c, IndexedSet) or isinstance(c, IndexedVariable) or isinstance(c, IndexedParameter):
-                    indices = 'i,j' #indices_list(c._forall)
-                    fd.write(f'${c.name}_{{{indices}}}$ & docs\\\\')
-
-            fd.write(r""" \\ \hline
-\end{tabular}
-\end{table}
-\end{document}
-""")
-            
-
-    
-class LatexExpressionWalker(BottomUpDepthFirstExpressionWalker):
+class ExpressionToLatexStringWalker(BottomUpDepthFirstExpressionWalker):
     def __init__(self):
         super().__init__()
-        self._symbols = dict()
-        self._str = ''
-        self._child_strs = list()
-        
-    def latex_expression(self, expr):
+        self._stack = []
+        self._allowed_math_functions = ['sin', 'cos', 'tan', 'log', 'exp']
+        self._binary_op_lookup = {
+            '+': '+',
+            '-': '-',
+            '*': r'\cdot',
+            '**': '^',
+            '==': '=',
+            '!=': r'\neq',
+            '<=': r'\leq',
+            '>=': r'\geq',
+        }
+
+    def expression_to_latex_string(self, expr):
+        assert self._stack == []
         self._walk(expr)
-        assert len(self._child_strs) == 1
-        return self._child_strs[0]
+        ret = self._stack.pop()
+        assert self._stack == []
+        return ret
     
+    @staticmethod
+    def forall_object_to_latex_string(forall_obj):
+        indices_string = '(' + ",".join([i.to_string() for i in forall_obj.indices_list()]) + ')'
+        set_strings = " \\times ".join([s.name for s in forall_obj.sets_list()])
+        return rf'\forall_{{{indices_string} \in {set_strings}}}'
+
     def _visit(self, expr):
-        if isinstance(expr, ComponentIndicesNode):
-            assert len(self._child_strs) == 0 or len(self._child_strs) == 1
-            indices = list(idx.name for idx in expr.indices)
-            indices = ','.join(indices)
-            self._child_strs.append(f'{expr.component.name}_{{{indices}}}')
-            
+        if isinstance(expr, BinaryExprNode):
+            right = self._stack.pop()
+            left = self._stack.pop()
+            if expr.operation in self._binary_op_lookup:
+                op = self._binary_op_lookup[expr.operation]
+                ret = f'{{{left}}} {op} {{{right}}}'
+            elif expr.operation == '/':
+                ret = f'\\frac{{{left}}}{{{right}}}'
+            else:
+                raise NotImplementedError(f'Binary operation {expr.operation} not supported in ExpressionToLatexStringWalker')
+            self._stack.append(ret)
+        elif isinstance(expr, UnaryExprNode):
+            arg = self._stack.pop()
+            if expr.operation not in self._allowed_math_functions:
+                ret = rf'\texttt{{{expr.operation}}}({arg})'
+            else:
+                ret = rf'\{expr.operation}({arg})'
+            self._stack.append(ret)
         elif isinstance(expr, ExprLeaf):
-            assert len(self._child_strs) == 0 or len(self._child_strs) == 1
-
-            self._child_strs.append(str(expr))
-        elif isinstance(expr, BinaryExprNode):
-            assert len(self._child_strs) == 2
-            opstr = self._latex_operation_str(expr.operation)
-            self._child_strs = [f'({self._child_strs[0]} {opstr} {self._child_strs[1]})']
+            if isinstance(expr, ComponentIndicesNode):
+                ret = rf'{expr.component.name}_{{{",".join([i.to_string() for i in expr.indices])}}}'
+            else:
+                ret = rf'{expr.to_string()}'
+            self._stack.append(ret)
         elif isinstance(expr, SumExprNode):
-            self._child_strs = [f'(\sum_{{i \in S}} {self._child_strs[0]})']
+            body = self._stack.pop()
+            ret = rf'\sum_{{{self.forall_object_to_latex_string(expr._forall)}}} ({body})'
+            self._stack.append(ret)
+        elif isinstance(expr, ProdExprNode):
+            body = self._stack.pop()
+            ret = rf'\prod_{{{self.forall_object_to_latex_string(expr._forall)}}} ({body})'
+            self._stack.append(ret)
+        elif isinstance(expr, Expression):
+            body = self._stack.pop()
+            ret = rf'({body})'
+            self._stack.append(ret)
         else:
-            raise NotImplementedError(f'Expression node {expr} of type {type(expr)} not supported in BottomUpDepthFirstExpressionWalker')
+            raise NotImplementedError(f'Expression node {expr} of type {type(expr)} not supported in ExpressionToLatexStringWalker')
         
-        print('visiting', str(expr))
+def expression_to_latex_string(expr):
+    return ExpressionToLatexStringWalker().expression_to_latex_string(expr)
 
-    def _latex_operation_str(self, op):
-        if op == '<=':
-            return '\le'
-        elif op == '>=':
-            return '\ge'
-        elif op == '*':
-            return ''
-        return op
+
+def variable_to_latex_string(var):
+
+    if var._domain == Domain.Reals:
+        dom_str =  f'\\mathbb{{R}}'
+    elif var._domain == Domain.Binary:
+        dom_str =  '\\{0, 1\\}'
+    else:
+        raise NotImplementedError(f'Domain {var._domain} not supported in variable_to_latex_string')
+    
+    if var._forall is None:
+        return f'{var.name} \\in {dom_str}'
+    else:
+        set_str = " \\times ".join([s.name for s in var._forall.sets_list()])
+        return f'{var.name} \\in {dom_str}^{{|{set_str}|}}'
+
+class LatexWriter(object):
+
+    def __init__(self):
+        self._expr_writer = ExpressionToLatexStringWalker()
+
+    def _write_model(self, model, fname):
+        with open(fname, 'w') as fd:
+            fd.writelines([f'\\documentclass{{article}}\n', f'\\usepackage{{amsmath}}\n', '\\usepackage{{amsfonts}}', f'\\begin{{document}}\n'])
+
+            fd.write(f'\\begin{{subequations}}\n')
+            fd.write(f'\\begin{{align}}\n')
+
+            fd.write(f'& \\text{{min}} && {self._expr_writer.expression_to_latex_string(model.objective)} &&& \\\\ \n')
+            constr = model.constraints[0]
+            fd.write(f'& \\text{{s.t.}} &&{self._expr_writer.expression_to_latex_string(constr._expr)}, &&& {self._expr_writer.forall_object_to_latex_string(constr._forall) if constr._forall else ""}')
+
+            for constr in model.constraints[1:]:
+                fd.write('\\\\ \n')
+                fd.write(f'& && {self._expr_writer.expression_to_latex_string(constr._expr)}, &&& {self._expr_writer.forall_object_to_latex_string(constr._forall) if constr._forall else ""}')
+            
+            fd.write('\\\\ \n')
+            fd.write('& && ')
+            for c in model.variables[:-1]:
+                fd.write(f'{variable_to_latex_string(c)}, ')
+            fd.write(f'{variable_to_latex_string(model.variables[-1])} &&&\n')
+
+            fd.write(f'\\end{{align}}\n')
+            fd.write(f'\\end{{subequations}}\n')
+
+            fd.writelines([f'\\end{{document}}'])
+            
+    # def write_model(self, fname, *args):
+    #     file_path = os.path.dirname(os.path.abspath(__file__))
+    #     templates_path = os.path.join(file_path, 'templates')
+    #     environment = Environment(loader=FileSystemLoader(templates_path))
+    #     template = environment.get_template("tex_template.tex")
+    #     symbstr = ''
+    #     constr = ''
+    #     for c in args:
+    #         if isinstance(c, Set) or isinstance(c, ScalarVariable) or isinstance(c, Parameter):
+    #             if c.is_scalar():
+    #                 symbstr += f'${c.name}$ & {c.doc}\\\\ \n'
+    #             else:
+    #                 indices = list(idx.name for idx in c._forall.indices_list())
+    #                 indices = ','.join(indices)
+    #                 symbstr += f'${c.name}_{{{indices}}}$ & {c.doc}\\\\ \n'
+    #         elif isinstance(c, Expression):
+    #             if c.is_scalar():
+    #                 expr = expression_to_latex_string(c.expr)
+    #                 constr += f'\\begin{{align}}\n{expr}\n\\end{{align}}\n'
+    #             else:
+    #                 expr = expression_to_latex_string(c.expr)
+    #                 constr += f'\\begin{{align}}\n{expr} \;\; \forall i \in S\n\\end{{align}}\n'
+    #         else:
+    #             raise NotImplementedError(f'Unknown expression {expr} of type {type(expr)} in LatexWriter')
+
+    #     content = template.render(symbols=symbstr, constraints=constr)
+    #     with open(fname, mode="w", encoding="utf-8") as texfile:
+    #         texfile.write(content)
+            
+
+    
+# class LatexExpressionWalker(BottomUpDepthFirstExpressionWalker):
+#     def __init__(self):
+#         super().__init__()
+#         self._symbols = dict()
+#         self._str = ''
+#         self._child_strs = list()
+        
+#     def latex_expression(self, expr):
+#         self._walk(expr)
+#         assert len(self._child_strs) == 1
+#         return self._child_strs[0]
+    
+#     def _visit(self, expr):
+#         if isinstance(expr, ComponentIndicesNode):
+#             assert len(self._child_strs) == 0 or len(self._child_strs) == 1
+#             indices = list(idx.name for idx in expr.indices)
+#             indices = ','.join(indices)
+#             self._child_strs.append(f'{expr.component.name}_{{{indices}}}')
+            
+#         elif isinstance(expr, ExprLeaf):
+#             assert len(self._child_strs) == 0 or len(self._child_strs) == 1
+
+#             self._child_strs.append(str(expr))
+#         elif isinstance(expr, BinaryExprNode):
+#             assert len(self._child_strs) == 2
+#             opstr = self._latex_operation_str(expr.operation)
+#             self._child_strs = [f'({self._child_strs[0]} {opstr} {self._child_strs[1]})']
+#         elif isinstance(expr, SumExprNode):
+#             self._child_strs = [f'(\sum_{{i \in S}} {self._child_strs[0]})']
+#         else:
+#             raise NotImplementedError(f'Expression node {expr} of type {type(expr)} not supported in BottomUpDepthFirstExpressionWalker')
+        
+#         print('visiting', str(expr))
+
+#     def _latex_operation_str(self, op):
+#         if op == '<=':
+#             return '\le'
+#         elif op == '>=':
+#             return '\ge'
+#         elif op == '*':
+#             return ''
+#         return op
