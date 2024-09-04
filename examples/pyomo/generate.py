55c2d6c7e265bada2f0668061b2dbe608151d278
whart222 <whart222@gmail.com>
2024-09-04 12:45:07 -0600

    Simplifying testing scripts
    Allowing for the execution of './run all'

--- examples/pyomo/generate.py
+++ examples/pyomo/generate.py
@@ -1,61 +1,65 @@
+import os
 import smoek as smk
 import smoek.core.tests.models as models
 from smoek.code.pyomo import generate
 
+if not os.path.exists("models"):
+    os.mkdir("models")
+
 model = models.small1()
-generate(model=model, outfile="small1.py")
+generate(model=model, outfile="models/small1.py")
 
 model = models.small2()
-generate(model=model, outfile="small2.py")
+generate(model=model, outfile="models/small2.py")
 
 model = models.small3()
-generate(model=model, outfile="small3.py")
+generate(model=model, outfile="models/small3.py")
 
 model = models.small4()
-generate(model=model, outfile="small4.py")
+generate(model=model, outfile="models/small4.py")
 
 model = models.small5()
-generate(model=model, outfile="small5.py")
+generate(model=model, outfile="models/small5.py")
 
 model = models.small6()
-generate(model=model, outfile="small6.py")
+generate(model=model, outfile="models/small6.py")
 
 model = models.testing1()
-generate(model=model, outfile="testing1.py")
+generate(model=model, outfile="models/testing1.py")
 
 model = models.testing2()
-generate(model=model, outfile="testing2.py")
+generate(model=model, outfile="models/testing2.py")
 
 #model = models.testing3()
-#generate(model=model, outfile="testing3.py")
+#generate(model=model, outfile="models/testing3.py")
 
 model = models.testing4()
-generate(model=model, outfile="testing4.py")
+generate(model=model, outfile="models/testing4.py")
 
 model = models.testing5()
-generate(model=model, outfile="testing5.py")
+generate(model=model, outfile="models/testing5.py")
 
 model = models.testing6()
-generate(model=model, outfile="testing6.py")
+generate(model=model, outfile="models/testing6.py")
 
 model = models.testing7()
-generate(model=model, outfile="testing7.py")
+generate(model=model, outfile="models/testing7.py")
 
 model = models.simple1()
-generate(model=model, outfile="simple1.py")
+generate(model=model, outfile="models/simple1.py")
 
 model = models.hs060()
-generate(model=model, outfile="hs060.py")
+generate(model=model, outfile="models/hs060.py")
 
 model = models.knapsack1(1)
-generate(model=model, outfile="knapsack1.py")
+generate(model=model, outfile="models/knapsack1.py")
 
 model = models.knapsack2(1)
-generate(model=model, outfile="knapsack2.py")
+generate(model=model, outfile="models/knapsack2.py")
 
 model = models.knapsack3()
-generate(model=model, outfile="knapsack3.py", data={"N"})
+generate(model=model, outfile="models/knapsack3.py", data={"N"})
 
 model = models.knapsack4()
-generate(model=model, outfile="knapsack4.py", data={"value", "weight", "max_weight", "ITEMS"})
+generate(model=model, outfile="models/knapsack4.py", data={"value", "weight", "max_weight", "ITEMS"})
 
