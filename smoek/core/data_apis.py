727a3a5b4a3a101b744afcee7cf45380e1195740
whart222 <whart222@gmail.com>
2026-09-21 10:32:29 -0600

    Commenting out bad code

--- smoek/core/data_apis.py
+++ smoek/core/data_apis.py
@@ -330,8 +330,8 @@ def JsonDataPortal(
 
     if schema == "coek":
         return JSONDataPortal_Coek(packed_data, filename=filename, lazyload=lazyload)
-    elif schema == "pyomo":
-        return JSONDataPortal_Pyomo(packed_data, filename=filename, lazyload=lazyload)
+    #elif schema == "pyomo":
+    #    return JSONDataPortal_Pyomo(packed_data, filename=filename, lazyload=lazyload)
 
     raise ValueError("Unexpected JSON schema: " + schema)
 
